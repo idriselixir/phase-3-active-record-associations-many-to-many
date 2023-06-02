fe29f528c9f99fed10de58854dd211fbd2590fdd
Idris Noor <idreesnoor150@gmail.com>
2023-06-02 08:55:27 +0300

    done

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,17 @@
+# app/models/game.rb
 class Game < ActiveRecord::Base
   has_many :reviews
+  has_many :users, through: :reviews
+end
+
+# app/models/user.rb
+class User < ActiveRecord::Base
+  has_many :reviews
+  has_many :games, through: :reviews
+end
+
+# app/models/review.rb
+class Review < ActiveRecord::Base
+  belongs_to :game
+  belongs_to :user
 end
